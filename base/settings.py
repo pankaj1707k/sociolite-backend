20e96a4b1562e678ad93886e97cd0c6414f2c6f7
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-19 21:03:57 +0530

    fix: use env vars for SECRET_KEY and DEBUG value

--- base/settings.py
+++ base/settings.py
@@ -12,18 +12,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialization for env vars
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c!)77$go#r=f-lm&)!vh09@ncjx+-g)(p$n9jdo!229w3^u6!w"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", bool)
 
 ALLOWED_HOSTS = []
 
