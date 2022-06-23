4eace51db719bcc1720d94d7091e256abdcecb2c
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-23 22:57:37 +0530

    feat: add custom auth backend

--- base/settings.py
+++ base/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication Model
+AUTH_USER_MODEL = "users.User"
+
+# Authentication Backend
+AUTHENTICATION_BACKENDS = ["users.backends.AuthBackend"]
---
+++ users/backends.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+UserModel = get_user_model()
+
+
+class AuthBackend(ModelBackend):
+    """
+    Authenticate using either username or email combined with the password.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if password is None:
+            return
+        try:
+            user = UserModel.objects.get(Q(username=username) | Q(email=username))
+        except UserModel.DoesNotExist:
+            # Derived from super().authenticate()
+            UserModel().set_password(password)
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
