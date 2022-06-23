143a8104d737f1620b3a9f4701a9c6ddf7146b0a
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-23 22:58:54 +0530

    test: create test for auth backend

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user, get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class AuthBackendTests(TestCase):
+    """
+    Test Authentication Backend
+    """
+
+    def setUp(self):
+        user = User.objects.create(username="username", email="user@test.com")
+        user.set_password("testpassword")
+        user.save()
+
+    def test_username_login(self):
+        self.assertFalse(get_user(self.client).is_authenticated)
+        self.client.login(username="username", password="testpassword")
+        self.assertTrue(get_user(self.client).is_authenticated)
+
+    def test_email_login(self):
+        self.assertFalse(get_user(self.client).is_authenticated)
+        self.client.login(username="user@test.com", password="testpassword")
+        self.assertTrue(get_user(self.client).is_authenticated)
