bc518d1c843787e9002b57b4ba97af938372d52d
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-23 22:59:47 +0530

    feat: modify admin site for user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from users.forms import UserCreationForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (_("Personal info"), {"fields": ("name",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+    add_form = UserCreationForm
+    list_display = ("id", "username", "email", "name", "is_staff")
+    search_fields = ("username", "email", "name")
+    ordering = ("-id",)
