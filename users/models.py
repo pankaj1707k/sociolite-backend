2722fe477bcdb79af740f7357510b228e8b670ed
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-23 22:55:34 +0530

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    A custom User model with `first_name` and `last_name` replaced with `name`.
+    The `email` field is set as unique and required.
+    """
+
+    first_name = None
+    last_name = None
+    name = models.CharField(_("name"), max_length=300, blank=True)
+    email = models.EmailField(_("email"), unique=True)
