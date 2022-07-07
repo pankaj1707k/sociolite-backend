db1aaf17040ce577fd01b8a6efa678ebcde40d97
pankaj1707k <pankaj.1707k@gmail.com>
2022-07-07 19:35:15 +0530

    docs: add swagger-ui auto-generating docs

--- base/settings.py
+++ base/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
+    "drf_yasg",
     "users.apps.UsersConfig",
     "posts.apps.PostsConfig",
 ]
--- base/urls.py
+++ base/urls.py
@@ -17,11 +17,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sociolite API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("users.urls")),
     path("api/post/", include("posts.urls")),
+    path(
+        "api/docs/spec.json/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "api/docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 # Handle media files in development environment
