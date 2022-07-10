0587567c388127205666794317522d3214b790e6
pankaj1707k <pankaj.1707k@gmail.com>
2022-07-10 17:09:54 +0530

    fix: change STATICFILES_STORAGE to fix deploy issue

--- base/settings.py
+++ base/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
